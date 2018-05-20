dac1c9970f16dc99bb772c276ac59c0e221bc7bd
QuantumSheep <quantumsheep@outlook.fr>
2018-05-20 02:41:02 +0200

    Added all modules to main export

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const SocketServer = require('./src/SocketServer');
 const WebSocketServer = require('./src/WebSocketServer');
+const WebSocketConnection = require('./src/WebSocketConnection');
+const SocketUtils = require('./src/SocketUtils');
 
-module.exports = { SocketServer, WebSocketServer };
+module.exports = {
+    SocketServer,
+    WebSocketServer,
+    WebSocketConnection,
+    SocketUtils
+};
